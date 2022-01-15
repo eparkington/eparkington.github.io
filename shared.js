f24f7da552b8c85ca435c6c0a1f5f7f34b452f9b
eparkington <81046295+eparkington@users.noreply.github.com>
2022-01-15 15:44:55 -0500

    update

--- shared.js
+++ shared.js
@@ -1,6 +1,9 @@
 let toggleButton = document.querySelector(".menu-button__bar");
 let mobileNav = document.querySelector(".mobile-nav");
 let changeButton = window.getComputedStyle(toggleButton, '::after');
+let wrappers = document.querySelectorAll('.wrapper');
+let projectTypes = document.querySelectorAll('.project-type');
+let projectExamples = document.querySelectorAll('.project-type__examples');
 
 toggleButton.addEventListener("click", function() {
     mobileNav.classList.toggle("show");
@@ -10,8 +13,29 @@ toggleButton.addEventListener("click", function() {
                                                    
 }
     );
+/*
+
+wrappers.forEach(wrapper => {
+    wrapper.addEventListener("mouseover", (e) => {
+        if(e.target.className === 'project-type') {
+            e.target.classList.add("hide");
+           
+      
+    }
+})
+    
+});
 
 
 
-
-
+wrappers.forEach(wrapper => {
+   wrapper.addEventListener("mouseout", (e) => {
+        if(e.target) {
+           e.target.classList.remove("hide");
+          
+        }
+    })
+    
+    
+});
+*/
